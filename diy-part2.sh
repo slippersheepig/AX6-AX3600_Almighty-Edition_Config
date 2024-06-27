127666db24bca4e33f0fe5b08d96fec3fe3fb4a5
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2024-06-27 20:19:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -46,4 +46,7 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 #rm -rf ../lean/luci-theme-argon
 
 #替换ath11k-firmware的默认Makefile
-wget https://github.com/slippersheepig/AX6-AX3600_Almighty-Edition_Config/raw/main/Makefile -O package/firmware/ath11k-firmware/Makefile
+wget https://github.com/slippersheepig/AX6-AX3600_Almighty-Edition_Config/raw/main/Makefile-ath11k -O package/firmware/ath11k-firmware/Makefile
+
+#替换uugamebooster的默认Makefile
+wget https://github.com/slippersheepig/AX6-AX3600_Almighty-Edition_Config/raw/main/Makefile-uugame -O feeds/packages/net/uugamebooster/Makefile
